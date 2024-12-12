f2792e1a2b58ceb464dfa712c32dbb4c5cb3d89d
chetan0019 <chetanbharangar893808@gmail.com>
2024-12-12 13:24:45 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Home from './pages/Home'
-import {} from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/clone" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   )
 }
 
